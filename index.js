d13825d2e215adf33b1bb19a5dead0e6990df438
Carlos Azaustre <cazaustre@gmail.com>
2015-04-23 16:48:20 +0200

    refactor callbacks

--- index.js
+++ index.js
@@ -3,6 +3,14 @@
 const http = require('http')
 const port = process.env.PORT || 3000
 
-const server = http.createServer()
+const server = http.createServer(onRequest)
 
-server.listen(port)
+server.listen(port, onListening)
+
+function onRequest (req, res) {
+  res.end('Hello io.js')
+}
+
+function onListening () {
+  console.log('Server running on port' + port)
+}
