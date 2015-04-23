5def28fb632520527468b2dc66c5156df0f8b98e
Carlos Azaustre <cazaustre@gmail.com>
2015-04-23 18:36:14 +0200

    use st to serve static files

--- index.js
+++ index.js
@@ -1,55 +1,15 @@
 'use strict'
 
 const http = require('http')
-const fs = require('fs')
-const path = require('path')
-const port = process.env.PORT || 3000
+const router = require('./router')
 
+const port = process.env.PORT || 3000
 const server = http.createServer()
 
-server.on('request', onRequest)
+server.on('request', router)
 server.on('listening', onListening)
 
 server.listen(port)
-
-function onRequest (req, res) {
-
-  let uri = req.url
-
-  if (uri.startsWith('/index') || uri === '/') return serveIndex(res)
-
-  if (uri.startsWith('/app.js')) return serveApp(res)
-
-  res.statusCode = 404
-  res.setHeader('Content-Type', text/plain)
-  res.end(`404 Not Found: ${ uri }`)
-}
-
-function serveIndex (res) {
-  let index = path.join(__dirname, 'public', 'index.html')
-  let rs = fs.createReadStream(index)
-
-  res.setHeader('Content-Type', 'text/html')
-  rs.pipe(res)
-
-  rs.on('error', function(err) {
-    res.setHeader('Content-Type', 'text/plain')
-    res.end(err.message)
-  })
-}
-
-function serveApp (res) {
-  let app = path.join(__dirname, 'public', 'app.js')
-  let rs = fs.createReadStream(app)
-
-  res.setHeader('Content-Type', 'text/javascript')
-  rs.pipe(res)
-
-  rs.on('error', function(err) {
-    res.setHeader('Content-Type', 'text/plain')
-    res.end(err.message)
-  })
-}
 
 function onListening () {
   console.log(`Server running on port ${port}`)
