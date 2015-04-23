652fc70b12fbb537bfcc87732d1d1d98e772ad9e
Carlos Azaustre <cazaustre@gmail.com>
2015-04-23 22:37:53 +0200

    declare functions to video convert

---
+++ helper/video.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const async = require('async')
+const EventEmitter = require('events').EventEmitter
+
+module.exports = function (images) {
+  let events = new EventEmitter()
+
+  async.series([
+    decodeImages,
+    createVideo,
+    encodeVideo,
+    cleanup
+  ], convertFinished)
+
+  // Decode images to files
+  function decodeImages (done) {
+    done()
+  }
+
+  // Create video from images with ffmpeg
+  function createVideo (done) {
+    done()
+  }
+
+  // Encode video
+  function encodeVideo (done) {
+    done()
+  }
+
+  // Cleanup temp folder
+  function cleanup (done) {
+    done()
+  }
+
+  // Convert finished
+  function convertFinished (err) {
+    setTimeout(function() {
+      events.emit('video', 'this will be the encoded video')
+    }, 500)
+  }
+
+  return events
+}
