d482cc54fab73b2bfa796f8e45ccfc8c13b45859
Carlos Azaustre <cazaustre@gmail.com>
2015-04-25 10:03:19 +0200

    add lveldb

--- database/index.js
+++ database/index.js
@@ -1,15 +1,25 @@
 'use strict'
 
+const level = require('level')
+const uuid = require('uuid')
+
 module.exports = function (options) {
   options = options || {}
+
+  const db = level('./messages.db')
 
   return {
     save: save,
     list: list
   }
-  
+
   function save (message, callback) {
-    callback()
+    let key = `message-${Date.now()}-${uuid.v4()}`
+    let options = {
+      valueEncoding: 'json'
+    }
+
+    db.put(key, message, options, callback)
   }
 
   function list (callback) {
